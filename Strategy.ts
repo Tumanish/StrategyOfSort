d7c831ca4cea952df5872ad3308d109cb4535d98
Tumanish <59100693+Tumanish@users.noreply.github.com>
2021-11-26 20:35:02 +0300

    SelectionSort
    Add SelectionSort

--- Strategy.ts
+++ Strategy.ts
@@ -175,11 +175,11 @@ namespace MySortProject {
 			this.sortStrategy.algorithm(this.dataArrayForSort);  // запустим алгоритм
 		}
 		print(): void{
-			console.log("");
+			console.log(" ");
 			console.log("Head : ",this.dataArrayForSort.get_front());
 			console.log("Tail : ",this.dataArrayForSort.get_back());
 			console.log("Length = ", this.dataArrayForSort.getLength());
-			console.log("");
+			console.log(" ");
 		}
 	}
 
@@ -204,11 +204,6 @@ namespace MySortProject {
 			this.dataList = _dataList; // так получили list
 			const length  = this.dataList.length;
 			
-			// //test Было
-			// for(let i=1;i<=length;i++){
-			// 	console.log(this.getDataElementByIndex(i));
-			// }
-			
 			for (let i = 1; i < length; i++) {				
 				for (let j = 1; j <=length - i; j++) {
 					if (this.getDataElementByIndex(j) > this.getDataElementByIndex(j + 1)) { //если правый меньше меняем местами
@@ -217,11 +212,6 @@ namespace MySortProject {
 				}
  			}
 
-			// //Стало
-			//  console.log(" ");
-			// for(let i=1;i<=length;i++){
-			// 	console.log(this.getDataElementByIndex(i));
-			// }
 			console.log("well done");
 		}
 		getDataElementByIndex(index: any): number {
@@ -272,8 +262,79 @@ namespace MySortProject {
 		}
 	}
 
+	class SelectionSort implements ISortStrategy{
+		dataList:List;
+		tempArray:number[] = [];
+		
+		algorithm(_dataList:List){
+			this.dataList = _dataList;
+			const length  = _dataList.getLength();
+
+			for (let i = 1; i <length ; i++){
+				let min = this.findMinElement(i,length);
+				this.repalceElement(i,min);
+			}
+		}
+		findMinElement(index,length){
+			let min = index;
+			for(let i = index+1; i<=length;i++){
+				if(this.getDataElementByIndex(min)>this.getDataElementByIndex(i)){
+					min = i;
+				}
+			}
+			return min;
+		}
+		getDataElementByIndex(index: any): number {
+			let res = 0;
+			this.tempArray = [];
+			//to do разобрать до i
+			for(let i = 1; i<=index+1;i++){
+				if(i==index){
+					res = this.dataList.get_front();
+				}else{
+				this.tempArray.push(this.dataList.get_front());	// пушим головной эллемент
+				this.dataList.remove_front();					// Удаляем готовной элемент
+				}
+			}
+			
+			//to do Собрать обратно до i
+			for(let i =1;i<=index;i++){
+				this.dataList.add_front(this.tempArray.pop());
+			}
+			return res;
+		}
+		repalceElement(index1: any, index2: any): void {
+			let data1:number;
+			let data2:number;
+			//сохраняем оригинал
+			data1 = this.getDataElementByIndex(index1);
+			data2 = this.getDataElementByIndex(index2);
+			//меняем местами
+			this.setElementByIndex(index1,data2);
+			this.setElementByIndex(index2,data1);
+		}
+		setElementByIndex(index:number,newData:number){
+			this.tempArray = [];
+			//разобрать лист до элемента и поменять на новый
+			for(let i = 1; i<=index;i++){
+				if(i==index){
+					this.dataList.remove_front();
+				}else{
+				this.tempArray.push(this.dataList.get_front());	// пушим головной эллемент
+				this.dataList.remove_front();					// Удаляем готовной элемент
+				}
+			}
+			//собрать обратно
+			this.dataList.add_front(newData);
+			for(let i = 1; i < index; i++){
+				this.dataList.add_front(this.tempArray.pop());
+			}
+		}
+	}
+
+
 	function start(size: number): void {
-		let generalObject = new General(new BubbleSort());	// кинем в конструктор тип сортировки, или поменяем в  методе setStrategy...
+		let generalObject = new General(new SelectionSort());	// кинем в конструктор тип сортировки, или поменяем в  методе setStrategy...
 		generalObject.createDataRandomShuffleArray(size);	// генератор и перемешивание рандомного
 		generalObject.createList();							// Создаст структуру, на основе сгенерированного массива, надо добавить проверку или совместить создание с генерацией
 		generalObject.print();
@@ -287,6 +348,7 @@ namespace MySortProject {
 		console.log(new Date());
 		console.log("finish ", fin - start," sec");
 		generalObject.print();								// чето-там в консоль
+		
 	}
 
 	console.clear();
